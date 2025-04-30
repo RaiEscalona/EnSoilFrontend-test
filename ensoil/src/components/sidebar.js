90500ab159f0eb0bd2f4aa7d0254852eecf0043b
JAVIERA PINTO <jpints@uc.cl>
2025-04-30 19:32:48 -0400

    style: Se modifica la sidebar según la tipografía

--- ensoil/src/components/sidebar.js
+++ ensoil/src/components/sidebar.js
@@ -4,9 +4,10 @@ import { Home, CirclePlus, History, ArrowLeftToLine } from "lucide-react";
 
 export default function Sidebar() {
   return (
-    <aside className="bg-quaternary w-20 h-screen text-white flex flex-col items-center pt-6">
-      <div className="bg-primary text-white w-12 h-12 rounded-full text-white flex flex-col items-center justify-center">ES</div>
-        <span className="text-black text-sm pt-2">ENSOIL</span>
+    <aside className="bg-quaternary w-21 h-screen text-white flex flex-col items-center pt-6">
+      <div className="bg-primary text-white w-12 h-12 rounded-full text-white flex flex-col items-center justify-center text-h3-medium"
+        >ES</div>
+        <span className="text-black text-h4 pt-2">ENSOIL</span>
       <nav className="flex flex-col gap-6 p-4 items-center flex-grow">
         <Link href="/acerca" className="text-primary flex flex-col items-center gap-1 hover:text-black pt-4">
           <CirclePlus 
@@ -17,13 +18,13 @@ export default function Sidebar() {
           <Home 
             size={30} 
             strokeWidth={1}/>
-          <span className="text-black text-sm">Inicio</span>
+          <span className="text-black text-h4">Inicio</span>
         </Link>
         <Link href="/contacto" className="text-black flex flex-col items-center gap-1 hover:text-primary pt-4">
           <History 
             size={30} 
             strokeWidth={1}/>
-          <span className="text-black text-sm">Recientes</span>
+          <span className="text-black text-h4">Recientes</span>
         </Link>
       </nav>
       <div>
