4ab3cdfd7d3f2e96f3cae45b197c7791e50a0b51
JAVIERA PINTO <jpints@uc.cl>
2025-05-06 14:36:35 -0400

    feat: se crea el componente header

---
+++ ensoil/src/components/header-ensoil.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function HeaderEnsoil() {
+  return (
+    <header className="bg-quaternary h-30">
+        <div className="flex justify-center items-center h-full p-4">
+            <Image
+                src="/Logo-EnSoil-Horizontal.png"
+                alt="Ensoil logo"
+                width={160}
+                height={0}
+                className="h-auto w-auto"
+            />
+        </div>
+    </header>
+  );
+}
