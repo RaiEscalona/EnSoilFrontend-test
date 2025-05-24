b256a46ad624c7fc1307888286c6ccee375f2a2f
JAVIERA PINTO <jpints@uc.cl>
2025-05-24 18:35:30 -0400

    feat: se agrega el componente restriction al layout de las páginas

--- ensoil/src/components/layouts/layoutWithSidebar.jsx
+++ ensoil/src/components/layouts/layoutWithSidebar.jsx
@@ -1,14 +1,17 @@
 'use client';
 
 import Sidebar from "@/components/sidebar";
+import { RestrictionAuth } from "../Authentication/RestrictionAuth";
 
 export default function WithSidebarLayout({ children }) {
   return (
     <div className="flex min-h-screen dark:bg-secondary">
+      <RestrictionAuth>
       <Sidebar />
       <main className="flex-1 ml-[21w] p-4 overflow-auto">
         {children}
       </main>
+      </RestrictionAuth>
     </div>
   );
 }
