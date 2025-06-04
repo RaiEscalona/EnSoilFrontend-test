6dabd03d8828575c2cf0f3d5b4eda1d33c43c1c6
JAVIERA PINTO <jpints@uc.cl>
2025-06-04 09:45:48 -0400

    feat: se muestra la tabla de metales suelo, conectada a back y en el dropdown

--- ensoil/src/app/projects/[id]/analysis/ground-metals-analysis.js
+++ ensoil/src/app/projects/[id]/analysis/ground-metals-analysis.js
@@ -1,39 +1,10 @@
 'use client'
 
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import api from '@/utils/axios';
-
 export default function GroundMetalsTable({data}) {
 
     if (!data) return <p>Datos no disponibles.</p>;
 
     const { info, analytes } = data;
-    // const { id } = useParams();
-    // const [isLoading, setIsLoading] = useState(true);
-
-    // useEffect(() => {
-    //     const fetchTableData = async () => {
-    //         try {
-    //             console.log(`🔄 Cargando datos de la tabla metales suelo del proyecto ${id}`);
-    //             const response = await api.get(`/${id}/groundMetals`);
-    //             console.log(`✅ Datos de la tabla metales suelo del proyecto ${id} cargado:`, response.data);
-
-    //             setDrillingPoint(response.data);
-    //         } catch (error) {
-    //             console.error(`❌ Error cargando punto de perforación ${id} proyecto ${id}:`, error.response?.data || error.message);
-    //             setAlertMessage(error.response?.data?.error || 'Error al cargar el punto de perforación.');
-    //         } finally {
-    //             setIsLoading(false);
-    //         }
-    //     };
-
-    //     fetchTableData();
-    // }, [id])
-
-    // if (isLoading) {
-    //     return <div className="loading">Cargando...</div>;
-    // }
 
     return (
     <div className="analysis-table-container">
@@ -47,12 +18,12 @@ export default function GroundMetalsTable({data}) {
                 <th key={point}>{point}</th>
               ))
             ) : (
-              <th colSpan={2}>No hay puntos de muestreo</th>
+              <th colSpan={2}>No hay datos de análisis</th>
             )}
           </tr>
         </thead>
         <tbody>
-            {info.map((point) =>
+            {info.length > 0 ? (info.map((point) => 
             point.sampleLogs.map((sample) => (
                 <tr key={sample.sampleLogTag}>
                     <th>{point.drillingPointTag}</th>
@@ -67,7 +38,8 @@ export default function GroundMetalsTable({data}) {
                         );
                     })}
                 </tr>
-            ))
+            )))) : (
+              <th colSpan={2}>No hay puntos de muestreo</th>
             )}
         </tbody>
       </table>
